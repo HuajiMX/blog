74f2d6cb200f24009050bf8a5017abddbe3b4f83
HuajiMC <huajimc@126.com>
2026-08-02 12:19:36 +0800

    优化自定义表情名称切割；新增两个新表情

--- wp-content/themes/Sakurairo/functions.php
+++ wp-content/themes/Sakurairo/functions.php
@@ -1480,7 +1480,7 @@ function get_custom_smilies_list()
 
             $file_base_name_part = explode('_', $file_base_name);
             $name = $file_base_name_part[0];
-            $type = isset($file_base_name_part[1]) ? ($file_base_name_part[1] == '1' ? 'meme' : 'emoji') : 'emoji';
+            $type = isset($file_base_name_part[count($file_base_name_part) - 1]) ? ($file_base_name_part[count($file_base_name_part) - 1] == '1' ? 'meme' : 'emoji') : 'emoji';
 
             if (in_array($file_extension, $custom_smilies_extension)) {
                 $custom_smilies_list[] = array(
